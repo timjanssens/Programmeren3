5d93a87cdc385f38e11188798b31d405e01d0b73
Tim Janssens <timjanssens81@gmail.com>
2020-09-22 21:07:41 +0200

    De les 4 hwapp afgewerkt

---
+++ hwapp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace hwapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+			Console.WriteLine("Hoe heet je?");
+			string naam = Console.ReadLine();
+			Console.WriteLine($"Hallo {naam}");
+            Console.ReadKey();
+        }
+    }
+}
