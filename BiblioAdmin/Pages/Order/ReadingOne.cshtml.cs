31dcbdfb219f989c05033bfd3d79ecdf1d54a254
Tim Janssens <timjanssens81@gmail.com>
2020-12-05 08:57:52 +0100

    Add Order Tab

---
+++ BiblioAdmin/Pages/Order/ReadingOne.cshtml.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BiblioAdmin.Pages.Order
+{
+    public class ReadingOneModel : PageModel
+    {
+        private readonly Bll.Student7Context DbContext;
+        public Bll.Order Order { get; set; }
+        public List<Bll.Order> OrderList { get; set; }
+
+        public ReadingOneModel(Bll.Student7Context dbContext)
+        {
+            this.DbContext = dbContext;
+        }
+        public void OnGet(int? id)
+        {
+            this.Order = DbContext.Order.SingleOrDefault(m => m.Id == id);
+            OrderList = DbContext.Order.ToList();
+        }
+    }
+}
