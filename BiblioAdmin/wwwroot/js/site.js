74f296e5e785c3a386737e7cc9f0cb36be83b3b4
Tim Janssens <timjanssens81@gmail.com>
2020-12-28 19:42:26 +0100

    Make some input fields only valid for numbers

--- BiblioAdmin/wwwroot/js/site.js
+++ BiblioAdmin/wwwroot/js/site.js
@@ -2,3 +2,18 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+
+
+function isNumberKey(evt) {
+    var charCode = (evt.which) ? evt.which : evt.keyCode
+    if (charCode > 31 && (charCode < 48 || charCode > 57))
+        return false;
+    return true;
+}
+
+function isNumberKeyDecimal(evt) {
+    var charCode = (evt.which) ? evt.which : evt.keyCode
+    if (charCode > 31 && (charCode != 46 && (charCode < 48 || charCode > 57)))
+        return false;
+    return true;
+}
