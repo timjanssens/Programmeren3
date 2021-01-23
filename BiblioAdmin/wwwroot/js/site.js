24d11702d771a73d37314252b2e3d9865ff71e6a
Tim Janssens <timjanssens81@gmail.com>
2021-01-23 11:09:38 +0100

    Allow only numbers using JS

--- BiblioAdmin/wwwroot/js/site.js
+++ BiblioAdmin/wwwroot/js/site.js
@@ -13,7 +13,7 @@ function isNumberKey(evt) {
 
 function isNumberKeyDecimal(evt) {
     var charCode = (evt.which) ? evt.which : evt.keyCode
-    if (charCode > 31 && (charCode != 46 && (charCode < 48 || charCode > 57)))
+    if (charCode > 31 && (charCode != 44 && (charCode < 48 || charCode > 57)))
         return false;
     return true;
 }
